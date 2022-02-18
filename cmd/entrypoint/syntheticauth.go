98c4add5afb1e395d9c5f671b4ce98ce92faba9d
Flynn <flynn@datawire.io>
2022-02-17 21:40:36 -0500

    Whoops, K8sSnapshot.annotations is a map, and (sigh) getNestedString is unreachable from here.
    Signed-off-by: Flynn <flynn@datawire.io>

--- cmd/entrypoint/syntheticauth.go
+++ cmd/entrypoint/syntheticauth.go
@@ -5,7 +5,7 @@ import (
 	//"github.com/datawire/dlib/derror"
 	"github.com/datawire/ambassador/v2/pkg/api/getambassador.io/v3alpha1"
 	"github.com/datawire/ambassador/v2/pkg/kates"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"github.com/datawire/ambassador/v2/pkg/snapshot/v1"
 )
 
 // This is a gross hack to remove all AuthServices using protocol_version: v2 only when running Edge-Stack and then inject an
@@ -48,20 +48,59 @@ func ReconcileAuthServices(ctx context.Context, sh *SnapshotHolder, deltas *[]*k
 		}
 	}
 
-	// Also loop over the annotations and remove authservices that are not v3
-	var annotations []*kates.Unstructured
-	for _, list := range sh.k8sSnapshot.Annotations {
-		for _, a := range list {
-			authAnnotation := a.(*kates.Unstructured)
-			if authAnnotation.GetKind() == "AuthService" && authAnnotation.GetAPIVersion() == "getambassador.io/v3alpha1" {
-				protoVersion := getNestedString(authAnnotation.Object, "spec", "protocol_version")
-				if protoVersion == "v3" {
-					annotations = append(annotations, authAnnotation)
+	// Also loop over the annotations and remove authservices that are not v3. We do
+	// this by looping over each entry in the annotations map, removing all the non-v3
+	// AuthService entries, and then removing any keys that end up with empty lists.
+	//
+	// keysToDelete tracks keys to delete, since I never trust deleting things from
+	// collections while iterating over them.
+	keysToDelete := make([]string, 0)
+
+	// OK. Loop over all the keys and their corresponding lists of annotations...
+	for key, list := range sh.k8sSnapshot.Annotations {
+		// ...and build up our edited list of things.
+		editedList := snapshot.AnnotationList{}
+
+		for _, obj := range list {
+			un := obj.(*kates.Unstructured)
+
+			if un.GetKind() != "AuthService" {
+				// This isn't an AuthService at all, so we'll keep it.
+				//
+				// XXX There's optimization to do here: if there are no AuthServices
+				// in our list, we needn't edit it at all. Something to circle back to
+				// later.
+				editedList = append(editedList, un)
+			} else {
+				// This _is_ an AuthService, so we'll check its protocol version.
+				// Anything other than v3 gets tossed.
+				//
+				// Note also that AuthServices other than getambassador.io/v3 cannot
+				// have a protocol_version, but whatever -- that'll be something not
+				// equal to v3, so it'll get tossed, and that's what we want.
+				spec := un.Object["spec"].(map[string]interface{})
+
+				if spec["protocol_version"] == "v3" {
+					// Whoa, it's a v3! Keep it.
+					editedList = append(editedList, un)
 				}
 			}
 		}
+
+		// Once here, is our editedList is empty?
+		if len(editedList) == 0 {
+			// Yes. Delete the whole key for this list.
+			keysToDelete = append(keysToDelete, key)
+		} else {
+			// Nope, not empty. Save the edited list.
+			sh.k8sSnapshot.Annotations[key] = editedList
+		}
 	}
-	sh.k8sSnapshot.Annotations = annotations
+
+	// Finally, delete any keys that ended up with empty lists.
+	for _, key := range keysToDelete {
+		delete(sh.k8sSnapshot.Annotations, key)
+	}
 
 	if len(authServices) == 0 && !syntheticAuthExists {
 		// There are no valid AuthServices with protocol_version: v3. A synthetic one needs to be injected.
